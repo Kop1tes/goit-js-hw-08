0378411062b1d8651f309c3261437c7205d359c4
Kop1tes <prsasha0@gmail.com>
2022-12-03 15:16:59 +0200

    add first change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,54 +4,23 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+// Change code below this line
 
 console.log(galleryItems);
-
 
 const gallery = document.querySelector('.gallery');
 const imgGallery = createGallery(galleryItems);
 
 gallery.insertAdjacentHTML('beforeend', imgGallery);
-console.log(createGallery);
 
 function createGallery(galleryItems) {
-  return galleryItems.map(({ preview, original, description }) => {
+    return galleryItems.map(({ preview, original, description }) => {
         return `
-<div class="gallery__item">
-<a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
     `
     }).join('');
 };
 
-gallery.addEventListener('click', onGalleryClick);
-
-
-
-function onGalleryClick(event) {
-    event.preventDefault();
-    if (event.target.nodeName != 'IMG') {
-        return;
-    }
-
-const instance = basicLightbox.create(
-  `<img src = "${event.target.dataset.source}" width = "800" height = "600"/>`, {
-    onShow: () => {document.addEventListener('keydown', onEscapeModalClose)},
-onClose: () => {document.removeEventListener("keydown", onEscapeModalClose)},
-  }
-  );
-
-  instance.show();
-  function onEscapeModalClose(evt) {
-  if (evt.code === "Escape") {
-    instance.close();
-  }
-};
-};
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle'
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('[name="email"]'),
+    message: document.querySelector('[name="message"]'),
+};
+
+const FEEDBACK_KEY = 'feedback-form-state';
+let formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextInput, 500));
+
+populateTextarea();
+
+function saveForm(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
+}
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    event.target.reset();
+
+    localStorage.removeItem(FEEDBACK_KEY);
+}
+
+function populateTextarea() {
+    const loadData = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+    if (loadData) {
+        refs.email.value = loadData.amail;
+        refs.message.value = loadData.message;
+    }
+}
 

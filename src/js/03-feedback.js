f415e3351737323ee876551d0f84775f7e868438
Kop1tes <prsasha0@gmail.com>
2022-12-04 19:34:11 +0200

    add second change

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,4 @@
 import throttle from 'lodash.throttle'
-
-
 const refs = {
     form: document.querySelector('.feedback-form'),
     email: document.querySelector('[name="email"]'),
@@ -11,29 +9,33 @@ const FEEDBACK_KEY = 'feedback-form-state';
 let formData = {};
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onTextInput, 500));
-
+refs.form.addEventListener('input', throttle(saveForm, 500));
 populateTextarea();
+
+// function onTextareaInput(evt) {
+//   let fg = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   formData = { ...fg, [evt.target.name]: evt.target.value };
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
 
 function saveForm(event) {
     formData[event.target.name] = event.target.value;
     localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
 }
 
-
 function onFormSubmit(event) {
     event.preventDefault();
-
     event.target.reset();
-
     localStorage.removeItem(FEEDBACK_KEY);
 }
 
 function populateTextarea() {
     const loadData = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
     if (loadData) {
-        refs.email.value = loadData.amail;
+        refs.email.value = loadData.email;
         refs.message.value = loadData.message;
     }
 }
+
+
 

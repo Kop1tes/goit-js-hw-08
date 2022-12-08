bed4629721733b5fa58a07957060c8f3d0fa98ea
Kop1tes <prsasha0@gmail.com>
2022-12-08 15:37:20 +0200

    add fifth change

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,100 @@
-import throttle from 'lodash.throttle';
-
-const refs = { 
-    form: document.querySelector('.feedback-form'), 
-    email: document.querySelector('.feedback-form input'), 
-    textarea: document.querySelector('.feedback-form textarea'), 
-}; 
-
-const LOCAL_STORAGE_KEY = 'feedback-form-state';
-let  formData = {};
 
 
-refs.form.addEventListener('submit', throttle(onFormSubmit, 500)); 
-refs.form.addEventListener('input', onInput);
+const throttler = require('lodash.throttle');
 
-
-function onInput(e) { 
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
-
+const refs = {
+    feedbackFormEl: document.querySelector(".feedback-form"),
+    inputEmailEl: document.querySelector("input[name=email]"),
+    textareaEl: document.querySelector("textarea[name=message]"),
 };
 
-populateData(); 
+let feedbackFormObject = {};
 
-function onFormSubmit(e) { 
-    e.preventDefault();
-    // console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))); 
-    e.currentTarget.reset(); 
-    localStorage.removeItem(LOCAL_STORAGE_KEY);
+refs.feedbackFormEl.addEventListener("input", throttler(onFeedbackFormInput, 500));
 
-};  
+function onFeedbackFormInput(e) {
+    feedbackFormObject[e.target.name] = e.target.value;
 
-function populateData() {
-    const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
-    const parseData = JSON.parse(savedData);
-    if (parseData) {
-        formData = parseData;
-        refs.textarea.value = parseData.message || '';
-        refs.email.value = parseData.email || '';
-        console.log(parseData);
+    let localStorageObject = JSON.stringify(feedbackFormObject);
+    localStorage.setItem("feedback-form-state", localStorageObject);
+}
+
+window.addEventListener("load", onFeedbackFormReplay);
+
+function onFeedbackFormReplay() {
+    if (!localStorage.getItem("feedback-form-state")) {
+    return;
     }
+
+    let parsedLocalStorageObject = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+    if (parsedLocalStorageObject.email) {
+    feedbackFormObject.email = parsedLocalStorageObject.email;
+    refs.inputEmailEl.value = parsedLocalStorageObject.email;
   }
+
+  if (parsedLocalStorageObject.message) {
+    feedbackFormObject.message = parsedLocalStorageObject.message;
+    refs.textareaEl.value = parsedLocalStorageObject.message;
+  }
+}
+
+
+
+// refs.feedbackFormEl.addEventListener("submit", onFeedbackFormReset);
+
+// function onFeedbackFormReset(e) {
+//   e.preventDefault();
+
+//   let parsedLocalStorageObject = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+//   if (parsedLocalStorageObject) {
+//     console.log(parsedLocalStorageObject);
+//   }
+
+//   refs.feedbackFormEl.reset();
+//   localStorage.removeItem("feedback-form-state");
+//   feedbackFormObject = {};
+// }
+
+// 2
+// import throttle from 'lodash.throttle';
+
+// const refs = { 
+//     form: document.querySelector('.feedback-form'), 
+//     email: document.querySelector('.feedback-form input'), 
+//     textarea: document.querySelector('.feedback-form textarea'), 
+// }; 
+
+// const LOCAL_STORAGE_KEY = 'feedback-form-state';
+// let  formData = {};
+
+// populateData(); 
+
+// refs.form.addEventListener('submit', onFormSubmit); 
+// refs.form.addEventListener('input', throttle(onInput, 500));
+
+
+// function onInput(event) { 
+//     formData[event.target.name] = event.target.value;
+//     let localStorageObject = JSON.stringify(formData);
+//     localStorage.setItem(LOCAL_STORAGE_KEY, localStorageObject);
+// };
+
+
+// function onFormSubmit(e) { 
+//     e.preventDefault(); 
+//     e.currentTarget.reset(); 
+//     localStorage.removeItem(LOCAL_STORAGE_KEY);
+// };  
+
+// function populateData() {
+//     const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+//     const parseData = JSON.parse(savedData);
+//     if (parseData) {
+//         formData = parseData;
+//         refs.textarea.value = parseData.message || '';
+//         refs.email.value = parseData.email || '';
+//     }
+//   }
   
